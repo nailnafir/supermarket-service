890cf72477529a2fc1ce0cb1e06bce7639a1d296
Nailul Firdaus <nail.nafir@gmail.com>
2022-12-19 21:05:57 +0700

    start project

---
+++ src/test/java/com/nailnafir/supermarket/SuperMarketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nailnafir.supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperMarketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
